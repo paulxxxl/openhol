b6cbbe439429d1c643ab61375a0c2b5b6e34d3ed
paulxxxl <ipavel@i.ua>
2016-07-07 18:01:26 +0300

    v 1.01 with simple 4.5

--- system/library/url.php
+++ system/library/url.php
@@ -1,39 +1,77 @@
 <?php
 class Url {
-	private $domain;
+	private $url;
 	private $ssl;
 	private $rewrite = array();
-
-	public function __construct($domain, $ssl = '') {
-		$this->domain = $domain;
+	
+	public function __construct($url, $ssl = '') {
+		$this->url = $url;
 		$this->ssl = $ssl;
 	}
-
+		
 	public function addRewrite($rewrite) {
 		$this->rewrite[] = $rewrite;
 	}
+		
+	public function link($route, $args = '', $connection = 'NONSSL') {
+        // SIMPLE START
+        global $config;
 
-	public function link($route, $args = '', $secure = false) {
-		if (!$secure) {
-			$url = $this->domain;
+        if (!empty($config) && method_exists($config, 'get') && $config->get('simple_settings')) {
+            $get_route = isset($_GET['route']) ? $_GET['route'] : (isset($_GET['_route_']) ? $_GET['_route_'] : '');
+                
+            if ($config->get('simple_replace_cart') && $route == 'checkout/cart' && $get_route != 'checkout/cart') {
+                $connection = 'SSL';
+                $route = 'checkout/simplecheckout';
+            }
+            
+            if ($config->get('simple_replace_checkout') && $route == 'checkout/checkout' && $get_route != 'checkout/checkout') {
+                $route = 'checkout/simplecheckout';
+            }
+            
+            if ($config->get('simple_replace_register') && $route == 'account/register' && $get_route != 'account/register') {
+                $route = 'account/simpleregister';
+            }   
+
+            if ($config->get('simple_replace_edit') && $route == 'account/edit' && $get_route != 'account/edit') {
+                $route = 'account/simpleedit';
+            }  
+
+            if ($config->get('simple_replace_address') && $route == 'account/address/update' && $get_route != 'account/address/update') {
+                $route = 'account/simpleaddress/update';
+            } 
+
+            if ($config->get('simple_replace_address') && $route == 'account/address/insert' && $get_route != 'account/address/insert') {
+                $route = 'account/simpleaddress/insert';
+            }  
+
+            if ($config->get('simple_replace_address') && $route == 'account/address/edit' && $get_route != 'account/address/edit') {
+                $route = 'account/simpleaddress/update';
+            } 
+
+            if ($config->get('simple_replace_address') && $route == 'account/address/add' && $get_route != 'account/address/add') {
+                $route = 'account/simpleaddress/insert';
+            }       
+        }
+        // SIMPLE END
+
+		if ($connection ===  'NONSSL' || !$connection) {
+			$url = $this->url;
 		} else {
-			$url = $this->ssl;
+			$url = $this->ssl;	
 		}
-
+		
 		$url .= 'index.php?route=' . $route;
-
+			
 		if ($args) {
-			if (is_array($args)) {
-				$url .= '&amp;' . http_build_query($args);
-			} else {
-				$url .= str_replace('&', '&amp;', '&' . ltrim($args, '&'));
-			}
+			$url .= str_replace('&', '&amp;', '&' . ltrim($args, '&')); 
 		}
-
+		
 		foreach ($this->rewrite as $rewrite) {
 			$url = $rewrite->rewrite($url);
 		}
-
+				
 		return $url;
 	}
 }
+?>
